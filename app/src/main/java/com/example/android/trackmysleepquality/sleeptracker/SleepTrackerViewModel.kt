94a1bd46f70a612db088f650f012aaef44174bda
tabasco-addict <43736991+tabasco-addict@users.noreply.github.com>
2020-06-11 18:29:16 -0700

    set up coroutines for database operations, click handler for start button

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -18,7 +18,10 @@ package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
+import kotlinx.coroutines.*
 
 /**
  * ViewModel for SleepTrackerFragment.
@@ -26,5 +29,64 @@ import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 class SleepTrackerViewModel(
         val database: SleepDatabaseDao,
         application: Application) : AndroidViewModel(application) {
+
+    // allows you to cancel coroutines started by this view model when it is no longer used
+    private var viewModelJob = Job()
+
+    // typically, use Dispatchers.Main for coroutines started in the viewModel;
+    // coroutines in the uiScope will run on the main thread
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private var tonight = MutableLiveData<SleepNight?>()
+
+    init {
+        initializeTonight()
+    }
+
+    private fun initializeTonight() {
+        //coroutine step 1:
+        //Launch a coroutine that runs on the main or UI thread, because the result affects the UI.
+        uiScope.launch {
+            //step 2:
+            //Call a suspend function to do the long-running work, so that you don't block the UI thread while waiting for the result
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    //step 3:
+    //Switch to the I/O context, so that the work can run in a thread pool that's optimized and set aside for these kinds of operations
+    private suspend fun getTonightFromDatabase(): SleepNight? {
+        return withContext(Dispatchers.IO) {
+            // call the database function to do the work.
+            var night = database.getTonight()
+            // If start and end times differ, the night has already been completed, so return null. Otherwise, return the night
+            if (night?.endTimeMilli != night?.startTimeMilli) {
+                night = null
+            }
+            night
+        }
+    }
+
+    //click handler for start button
+    fun onStartTracking() {
+        uiScope.launch {
+            val newNight = SleepNight()
+            insert(newNight)// not the Dao insert method; defined below:
+            tonight.value = getTonightFromDatabase()
+        }
+
+    }
+
+    private suspend fun insert(night: SleepNight) {
+        withContext(Dispatchers.IO) {
+            database.insert(night)
+        }
+    }
+
+    // when viewModel is destroyed, jobs are cancelled
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
 }
 
